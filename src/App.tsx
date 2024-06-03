b6a3b2a4de9cf9749eab74f2bfa9d16172ac23a6
SebastianHdzMiranda <sebashm68@gmail.com>
2024-06-03 14:24:41 -0600

    Añadir calorieTraker

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useReducer } from "react"
 import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activity-reducers"
 import ActivityList from "./components/ActivityList";
+import CalorieTraker from "./components/CalorieTraker";
 
 function App() {
 
@@ -17,13 +18,13 @@ function App() {
   return (
     <>
       <header className="bg-lime-600 py-4">
-          <div className="flex justify-between container items-center">
-            <h1 className="text-center md:text-lg text-white uppercase font-bold">
+          <div className="flex justify-between px-4 sm:max-w-2xl lg:max-w-5xl mx-auto items-center">
+            <h1 className="text-center md:text-2xl text-white uppercase font-bold">
               Contador de Calorias
             </h1>
 
             <button 
-              className="p-2 text-white bg-slate-600 rounded-lg hover:bg-slate-700 transition-colors text-sm uppercase font-bold disabled:opacity-10"
+              className="p-2 text-white bg-slate-600 rounded-lg hover:bg-slate-700 transition-colors text-sm uppercase font-bold disabled:opacity-10 border-none"
               onClick={ ()=> dispatch({type: "reset-activity"})}
               disabled={!canResetApp()}
             >
@@ -33,7 +34,7 @@ function App() {
       </header>
 
       <section className="bg-lime-500 py-20">
-        <div className="container">
+        <div className="px-4 sm:max-w-2xl lg:max-w-5xl mx-auto">
           <Form 
             datosState={ state }
             dispatch={ dispatch }
@@ -41,7 +42,15 @@ function App() {
         </div>
       </section>
 
-      <section className="container py-10 lg:max-w-7xl">
+      <section className="bg-gray-800 py-10">
+        <div className="px-4 sm:max-w-2xl lg:max-w-5xl mx-auto">
+          <CalorieTraker 
+            activities={state.activities}
+          />
+        </div>
+      </section>
+
+      <section className="px-4 sm:max-w-2xl lg:max-w-5xl mx-auto py-10">
         <ActivityList 
           activities={state.activities}
           dispatch={dispatch}
